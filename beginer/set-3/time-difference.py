91486c72396314f1473ea9cb65dd55aaa74af9ed
Rajeshkumar Animuthu <n20084753@gmail.com>
2018-01-27 19:01:26 +0530

    difference between two given times using python

---
+++ beginer/set-3/time-difference.py
@@ -0,0 +1,6 @@
+h1, m1 = [int(x) for x in raw_input().split(" ")]
+h2, m2 = [int(x) for x in raw_input().split(" ")]
+
+timeDiffInMinutes = abs((h1 * 60 + m1) - (h2 * 60 + m2))
+
+print(str(timeDiffInMinutes / 60) +" "+ str(timeDiffInMinutes % 60))
