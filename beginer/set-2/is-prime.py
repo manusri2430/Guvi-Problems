dcb381e6eec6ab5c5e488156b5e74fb56053a6ed
Rajeshkumar Animuthu <n20084753@gmail.com>
2018-01-28 12:28:48 +0530

    fixed prime number edge case for 0 and 1

--- beginer/set-2/is-prime.py
+++ beginer/set-2/is-prime.py
@@ -1,9 +1,13 @@
 import math
 
 def isPrime(n):
+	if (n == 0 or n == 1):
+		print("No")
+		return
+
 	m = int(math.ceil(n**(0.5)))
 
-	for i in range(2, m):
+	for i in range(2, m+1):
 		if(n % i == 0):
 			print("No")
 			return
--- beginer/set-7/66-is-prime.py
+++ beginer/set-7/66-is-prime.py
@@ -1,7 +1,10 @@
 import math
 def isPrime(n):
-	m = int(math.ceil(n**(0.5)))	
-	for i in range(2, m):
+	if (n == 0 or n == 1):
+		return False
+
+	m = int(math.ceil(n**(0.5)))
+	for i in range(2, m+1):
 		if n % i == 0:
 			return False
 	return True
